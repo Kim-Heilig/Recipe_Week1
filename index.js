24b7716cdaeaf1c6cd858f490230fb36a05bbcef
Kim Heilig <kimmarieheilig@gmail.com>
2024-07-22 10:38:49 -0400

    adjustments to layouts right/left.  Needs improvement

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const p = document.createElement('p');
-p.innerText = "As a single working mom with limited cooking skills, I initially saw the necessity of preparing healthy meals with my kids as a daunting task. However, I quickly realized this was an opportunity to transform mealtime into quality bonding and valuable learning experiences. Teaching my two children, Mary and Elliott, to enjoy cooking from a young age not only helps me manage our meals more efficiently but also fosters healthy habits and valuable life skills. Together, we explore simple recipes that are nutritious and fun to make, turning what could be a chore into a creative and engaging activity. Beyond learning to chop vegetables or stir ingredients, they are developing patience, cooperation, and a genuine interest in food and nutrition. This early exposure not only ensures they grow up with a healthy relationship with food but also strengthens our family bond as we work together toward a common goal each day.";
-document.body.append(p);
+const p = document.createElement('h1');
+p.innerText = "Little Sous Chefs";
+document.body.prepend(p);
 
-p.style.color = "white";
+
+const myDiv = document.getElementById("myDiv");
+//create checkbox element
+let checkbox = document.createElement("input");
+
+//assigning the attributes to created checkbox
+checkbox.type = "checkbox";
+checkbox.name = "name";
+checkbox.value = "value";
+checkbox.id = "id";
+
+// create label for checkbox
+var label = document.createElement("label");
+
+//append the created text to created label tag
+label.appendChild(
+  document.createTextNode("This create the label for checkbox."));
+
+  myDiv.appendChild(checkbox);
+  myDiv.appendChild(label);
+
+  function loadGoogleFont(font){
+    const link = document.createElement()
+  }
