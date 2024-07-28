c487ccaf6516d3dbcea7ac5438e0b6eb66102a30
Kim Heilig <kimmarieheilig@gmail.com>
2024-07-28 18:28:41 -0400

    changes made to the linked html pages and checkboxes.  Need to work on more DOM manipulations

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ p.classList.add("custom_font");
 p.innerText = "Little Sous Chefs";
 document.body.prepend(p);
 
-
 //
 
+// NOTES TO SELF: the code below is interesting and I only partly understand it.  It attempts to listen to the checklist for changes and apply code onto object e, given the conditions specified.
 
+// document.getElementById("checklist").addEventListener("change", function (e) {
+//   if (e.target.type === "checkbox") {
+//     e.target.nextElementSibling.style.textDecoration = e.target.checked
+//       ? "line-through"
+//       : "none";
+//     e.target.nextElementSibling.style.color = e.target.checked ? "#888" : "";
+//   }
+// });
 
 // these did not work
 
